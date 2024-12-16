fdea389cfea29de1fd7883258e92d8a8040b222b
Gianpietro <panico.gianpietro@gmail.com>
2024-12-16 19:12:29 +0100

    first commit

---
+++ PomoTracker/PomoTracker/PomoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoTrackerApp.swift
+//  PomoTracker
+//
+//  Created by Gianpietro Panico on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PomoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
